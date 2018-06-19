910586a6f0e5ba934d6c508cb27afecc076350a9
Alejandro Gonzalez Jimenez <alexgj@um.es>
2018-06-19 13:13:14 +0200

    Key store added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 const Provider = require('oidc-provider');
 
-// new Provider instance with no extra configuration, will run in default, just needs the issuer
-// identifier, uses data from runtime-dyno-metadata heroku here
 const oidc = new Provider('http://localhost:3000');
+
+const keystore = require('./keystore.json');
+
+// http:/localhost:3000/auth?client_id=foo&response_type=code&scope=openid
 
 // initialize with no keystores, dev ones will be provided
 oidc.initialize({
+  keystore,
   // just a foobar client to be able to start an Authentication Request
   clients: [{ client_id: 'foo', client_secret: 'bar', redirect_uris: ['http://lvh.me/cb'] }],
 }).then(() => {
